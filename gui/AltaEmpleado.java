74b03f1eab9cf9c34b58145e58bc92c2fa5c9885
Jose19-e <jose_sj27@hotmail.com>
2021-03-12 17:29:14 -0600

    version 3

--- gui/AltaEmpleado.java
+++ gui/AltaEmpleado.java
@@ -19,6 +19,7 @@ import mx.unam.aragon.fes.persistencia.ArchivoEmpleado;
 public class AltaEmpleado extends javax.swing.JFrame {
 
     private ArrayList<Empleado> lista;
+    private int indice;
 
     /**
      * Creates new form AltaEmpleado
@@ -26,6 +27,7 @@ public class AltaEmpleado extends javax.swing.JFrame {
     public AltaEmpleado() {
         initComponents();
         lista = new ArrayList<Empleado>();
+        indice = 0;
     }
 
     /**
@@ -75,6 +77,7 @@ public class AltaEmpleado extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
+        jLabel14 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -310,8 +313,18 @@ public class AltaEmpleado extends javax.swing.JFrame {
         });
 
         jButton2.setText("Cargar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jButton3.setText(">>");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
 
         jButton4.setText("<<");
 
@@ -329,6 +342,9 @@ public class AltaEmpleado extends javax.swing.JFrame {
                 jButton6MouseClicked(evt);
             }
         });
+
+        jLabel14.setFont(new java.awt.Font("Tahoma", 3, 18)); // NOI18N
+        jLabel14.setText("0");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -351,13 +367,19 @@ public class AltaEmpleado extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(460, 460, 460)
+                .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel14)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2)
@@ -365,7 +387,7 @@ public class AltaEmpleado extends javax.swing.JFrame {
                     .addComponent(jButton4)
                     .addComponent(jButton5)
                     .addComponent(jButton6))
-                .addGap(51, 51, 51))
+                .addGap(53, 53, 53))
         );
 
         pack();
@@ -428,6 +450,47 @@ public class AltaEmpleado extends javax.swing.JFrame {
         }
 
     }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+        int respuesta = JOptionPane.showConfirmDialog(this, "Esta acción borrara los datos actuales. ¿Estás seguro de continuar?");
+        if (respuesta == 0) {
+            // leer el archivo
+            // Subir al Array list actual
+            // El primer elemento del Array List se pondrá en el formulario
+            ArchivoEmpleado persistencia = new ArchivoEmpleado();
+            JFileChooser jfc = new JFileChooser();
+            jfc.showOpenDialog(this);
+            persistencia.setArchivo(jfc.getSelectedFile().getAbsolutePath());
+            this.lista = persistencia.leerEmpleados();
+            this.limpiarFormulario();
+            this.indice = 0;
+            this.llenarFormulario(this.indice);
+
+        } else {
+        }
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+//falta hacer control de tamaño
+        if (this.indice < this.lista.size() - 1) {
+            this.indice++; // this.indice = this.indice +1;
+        } else {
+            this.indice = 0;
+        }
+        this.jLabel14.setText("" + this.indice);
+        llenarFormulario(indice);
+    }//GEN-LAST:event_jButton3MouseClicked
+    private void llenarFormulario(int indice) {
+        if (lista.size() > 0) {
+            Empleado e = lista.get(indice);
+            this.jTextField1.setText(e.getNombre());
+            this.jTextField12.setText(e.getApPaterno());
+            this.jTextField3.setText(e.getApMaterno());
+            this.jTextField4.setText("" + e.getEdad());
+            this.jTextField5.setText(e.getCurp());
+        }
+    }
 
     private void limpiarFormulario() {
         this.jTextField1.setText("");
@@ -497,6 +560,7 @@ public class AltaEmpleado extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
